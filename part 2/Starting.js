5fabdbea46fefe55bfcbcb18fbd436b557ae5adc
Neel Patel <neel.patel@metropolia.fi>
2021-07-27 15:55:31 +0300

    const object1 = {   name: 'Arto Hellas',   age: 35,   education: 'PhD', }
    const object2 = {
      name: 'Full Stack web application development',
      level: 'intermediate studies',
      size: 5,
    }
    
    const object3 = {
      name: {
        first: 'Dan',
        last: 'Abramov',
      },
      grades: [2, 3, 5, 3],
      department: 'Stanford University',
    }
    
    Create Part 2

---
+++ part 2/Starting.js
@@ -0,0 +1,43 @@
+const x = 1;
+let y = 5;
+
+console.log(x,y);
+y += 10
+
+console.log(x,y);
+y = 'sometext'
+
+console.log(x,y)
+x = 4
+
+const t = [1, -1, 3]
+
+t.push(5)
+
+console.log(t.length) // 4 is printed
+console.log(t[1])     // -1 is printed
+
+t.forEach(value => {
+  console.log(value)  // numbers 1, -1, 3, 5 are printed, each to own line
+})     
+
+const object1 = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+  }
+  
+  const object2 = {
+    name: 'Full Stack web application development',
+    level: 'intermediate studies',
+    size: 5,
+  }
+  
+  const object3 = {
+    name: {
+      first: 'Dan',
+      last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Stanford University',
+  }
